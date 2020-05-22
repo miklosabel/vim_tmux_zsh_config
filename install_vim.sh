a0ab9bfb7217f313e160c66d17a1c7c37fa53090
miklosabel <miklosabel@gmail.com>
2020-05-22 09:36:53 +0200

    Update install_vim.sh
    no need to install when you cloned this repo

--- install_vim.sh
+++ install_vim.sh
@@ -11,7 +11,6 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # install vundle
-apt install -y git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
