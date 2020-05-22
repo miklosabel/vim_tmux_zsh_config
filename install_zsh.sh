3524198893bca17849f67db58b25992d2210bc70
miklosabel <miklosabel@gmail.com>
2020-05-22 09:37:25 +0200

    Update install_zsh.sh
    no need to install git after you cloned this repo

--- install_zsh.sh
+++ install_zsh.sh
@@ -2,7 +2,6 @@
 
 apt install -y zsh
 apt install -y curl  # TODO check if installed
-apt install -y git   # TODO check if installed
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo -n "Now you can copy zshrc to ~"
